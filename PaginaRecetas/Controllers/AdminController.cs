33b1021af797d9dfb76c906f3bce30a0c6c05734
Raúl Manzanera Medina <raul.manzaneram@uanl.edu.mx>
2025-05-19 12:52:38 -0600

    Se agrego el controlador para Admin.

---
+++ PaginaRecetas/Controllers/AdminController.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PaginaRecetas.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: AdminController
+        public ActionResult Estadisticas()
+        {
+            return View();
+        }
+        public ActionResult GestionUsuarios()
+        {
+            return View();
+        }
+        public ActionResult GestionFiltros()
+        {
+            return View();
+        }
+        public ActionResult GestionElementosDestacados()
+        {
+            return View();
+        }
+        public ActionResult Reportes()
+        {
+            return View();
+        }
+        public ActionResult RecetasNuevas()
+        {
+            return View();
+        }
+
+        public ActionResult FiltroTipo()
+        {
+            return View();
+        }
+
+        public ActionResult FiltroComplejidad()
+        {
+            return View();
+        }
+
+        public ActionResult FiltroTiempo()
+        {
+            return View();
+        }
+
+        public ActionResult FiltroRegion()
+        {
+            return View();
+        }
+
+
+        // GET: AdminController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AdminController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AdminController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AdminController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AdminController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AdminController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AdminController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
