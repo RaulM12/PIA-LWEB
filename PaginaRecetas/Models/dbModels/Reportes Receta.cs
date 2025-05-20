4b1bb3192d98cc261ad1cb0ad749931d3c5f03d9
Raúl Manzanera Medina <raul.manzaneram@uanl.edu.mx>
2025-05-19 23:27:30 -0600

    Se agrego la carpeta dbModels.

---
+++ PaginaRecetas/Models/dbModels/Reportes Receta.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace PaginaRecetas.Models.dbModels;
+
+[Table("reportes_recetas")]
+public partial class ReportesReceta
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("receta_id")]
+    public int RecetaId { get; set; }
+
+    [Column("motivo_reporte", TypeName = "text")]
+    public string MotivoReporte { get; set; } = null!;
+
+    [Column("fecha_reporte")]
+    public DateOnly FechaReporte { get; set; }
+
+    [Column("estatus_id")]
+    public int EstatusId { get; set; }
+
+    [ForeignKey("EstatusId")]
+    [InverseProperty("ReportesReceta")]
+    public virtual EstatusReporte Estatus { get; set; } = null!;
+
+    [ForeignKey("RecetaId")]
+    [InverseProperty("ReportesReceta")]
+    public virtual Receta Receta { get; set; } = null!;
+}
---
+++ PaginaRecetas/Models/dbModels/Tiempo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace PaginaRecetas.Models.dbModels;
+
+[Table("tiempo")]
+[Index("Rango", Name = "UQ__tiempo__0506009D4497A278", IsUnique = true)]
+public partial class Tiempo
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("rango")]
+    [StringLength(25)]
+    [Unicode(false)]
+    public string Rango { get; set; } = null!;
+
+    [InverseProperty("RangoTiempoNavigation")]
+    public virtual ICollection<Receta> Receta { get; set; } = new List<Receta>();
+}
