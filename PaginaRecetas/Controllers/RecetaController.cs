1f52c303ef26446e2cbe0512d2c9feb6edcd74c0
Raúl Manzanera Medina <raul.manzaneram@uanl.edu.mx>
2025-05-25 23:58:44 -0600

    Se agrego RecetaController

---
+++ PaginaRecetas/Controllers/RecetaController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PaginaRecetas.Data;
+using System.Threading.Tasks;
+using static PaginaRecetas.Data.ApplicationDbContext;
+
+namespace PaginaRecetas.Controllers
+{
+    public class RecetaController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RecetaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Mostrar todas las recetas
+        public IActionResult Index()
+        {
+            var recetas = _context.receta.ToList();
+            return View(recetas);
+        }
+
+        // GET: Formulario para crear
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        // POST: Crear receta
+        [HttpPost]
+        public async Task<IActionResult> Crear(recetas model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.fecha_creacion = DateTime.Now;
+                model.visitas = 0;
+                _context.receta.Add(model);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+    }
+}
